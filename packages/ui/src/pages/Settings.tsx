4ae3b225793d9a44c6985fe1bd7ef662d7eab201
Twisted Analytics <187667211+TwistedAnalytics@users.noreply.github.com>
2026-02-01 18:14:04 -0700

    Import useWallet hook in Settings component

--- packages/ui/src/pages/Settings.tsx
+++ packages/ui/src/pages/Settings.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useWallet } from '../hooks/useWallet';
 import { useNavigation } from '../contexts/NavigationContext';
 
 export const Settings: React.FC = () => {
