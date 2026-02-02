5660b6b84e2260682c4ffc2fa78323d35300b947
Twisted Analytics <187667211+TwistedAnalytics@users.noreply.github.com>
2026-02-01 18:14:55 -0700

    Comment out useNavigation and useNavigate exports
    Comment out the export of useNavigation and useNavigate.

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,4 +1,4 @@
 export { useWallet } from './useWallet';
 export { useNFTs } from './useNFTs';
 export { useMessaging } from './useMessaging';
-export { useNavigation, useNavigate } from './useNavigate';
+//export { useNavigation, useNavigate } from './useNavigate';
